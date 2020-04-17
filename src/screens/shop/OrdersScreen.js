beb84fe4cf46ff15268e6b3b67d1a0c8428517c6
Carlos Avina <carlos.avina@cbqasolutions.com>
2020-04-16 23:03:33 -0500

    fetching stored orders

--- src/screens/shop/OrdersScreen.js
+++ src/screens/shop/OrdersScreen.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { FlatList, Text, Platform } from 'react-native';
-import { useSelector } from 'react-redux';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import React, { useState, useEffect } from "react";
+import {
+  FlatList,
+  Text,
+  Platform,
+  ActivityIndicator,
+  View,
+  StyleSheet,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import * as orderActions from "../../store/actions/orders";
+import Colors from "../../constants/Colors";
 
-import HeaderButton from '../../components/UI/HeaderButton';
-import OrderItem from '../../components/shop/OrderItem';
+import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
 
-const OrdersScreen = props => {
-  const orders = useSelector(state => state.orders.orders);
+const OrdersScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
 
+  const dispatch = useDispatch();
+  const orders = useSelector((state) => state.orders.orders);
+
+  useEffect(() => {
+    setIsLoading(true);
+    dispatch(orderActions.fetchOrders());
+    setIsLoading(false);
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.accent} />
+      </View>
+    );
+  }
   return (
     <FlatList
       data={orders}
-      keyExtractor={item => item.id}
-      renderItem={itemData => (
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
         <OrderItem
           amount={itemData.item.totalAmount}
           date={itemData.item.readableDate}
@@ -24,21 +49,28 @@ const OrdersScreen = props => {
   );
 };
 
-OrdersScreen.navigationOptions = navData => {
+OrdersScreen.navigationOptions = (navData) => {
   return {
-    headerTitle: 'Your Orders',
+    headerTitle: "Your Orders",
     headerLeft: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title="Menu"
-          iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
           onPress={() => {
             navData.navigation.toggleDrawer();
           }}
         />
       </HeaderButtons>
-    )
+    ),
   };
 };
 
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 export default OrdersScreen;
